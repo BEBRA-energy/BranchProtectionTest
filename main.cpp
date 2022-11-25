02c8f7a34ce27709bde9ed7789efe68e5ee241ed
MicroMalekul <ilyakrasnik03@gmail.com>
2022-11-25 09:03:03 +0300

    я бiндера

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
-    std::cout << "sakdasjdkj!" << std::endl;
-    return 0;
+    std::cout << "bindera!" << std::endl;
+    int x = 15;
+    while (x--) {
+        for (int i = 0; i <= x; i++) {
+            std::cout << i << '\t';
+        }
+        std::cout << '\n';
+    }
+    return 12;
 }
